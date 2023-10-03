80418b77e755f9201879d133310f6f3a64abba89
Constantine Zorge <wolverineo250kr@yandex.ru>
2023-10-04 01:40:43 +0500

    Update AboutView.php

--- Application/Views/AboutView.php
+++ Application/Views/AboutView.php
@@ -1,5 +1,5 @@
 <?php
-
+// Подключение необходимых файлов
 require_once 'vendor/autoload.php';
 require_once 'Application/Core/View.php';
 
@@ -7,9 +7,13 @@ class AboutView extends View
 {
     public function render($data)
     {
+       // Создание загрузчика для шаблонов Twig
         $loader = new \Twig\Loader\FilesystemLoader('Application/Views/Templates');
+
+       // Создание экземпляра Twig
         $twig = new \Twig\Environment($loader);
 
+      // Рендеринг шаблона с передачей данных
         echo $twig->render('about.twig', [
             'title' => 'О нас',
             'data' => $data,
