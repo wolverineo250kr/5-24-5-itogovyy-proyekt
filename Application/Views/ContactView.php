4f755923c3d12a3d0f5b85469d2f81c4c293ba26
Constantine Zorge <wolverineo250kr@yandex.ru>
2023-10-04 01:41:47 +0500

    Update ContactView.php

--- Application/Views/ContactView.php
+++ Application/Views/ContactView.php
@@ -1,11 +1,16 @@
 <?php
+// Подключение необходимых файлов
 require_once 'Application/Core/View.php';
 
 class ContactView extends View {
     public function render($data) {
+        // Создание загрузчика для шаблонов Twig
         $loader = new \Twig\Loader\FilesystemLoader('Application/Views/Templates');
+        
+        // Создание экземпляра Twig
         $twig = new \Twig\Environment($loader);
 
+         // Рендеринг шаблона с передачей данных
         echo $twig->render('contact.twig', [
             'title' => 'Контакты',
             'data' => $data,
