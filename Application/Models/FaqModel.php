ecebc72fc9e4efd19188ec12092908aa0f0f188b
Constantine Zorge <wolverineo250kr@yandex.ru>
2023-10-04 01:33:30 +0500

    Update FaqModel.php

--- Application/Models/FaqModel.php
+++ Application/Models/FaqModel.php
@@ -7,7 +7,7 @@ class FaqModel extends Model
     public function getData()
     {
         $data = 'Часто задаваемые вопросы';
-        // Ваш код для получения данных
+       
         return $data;
     }
 }
